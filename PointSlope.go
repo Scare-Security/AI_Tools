57aa1c4b7b0811066d350a51067638b07e78a286
Totally_Not_A_Haxxer <87203697+ArkAngeL43@users.noreply.github.com>
2022-12-08 00:01:16 -0500

    Create PointSlope.go

---
+++ PointSlope.go
@@ -0,0 +1,30 @@
+To calculate a point-slope formula in Go, you can use the built-in math package to perform the necessary mathematical operations.
+
+Here's an example of how you can calculate a point-slope formula in Go:
+
+Copy code
+package main
+
+import (
+  "fmt"
+  "math"
+)
+
+func main() {
+  // Define the coordinates of the point and the slope of the line
+  x1 := 3.0
+  y1 := 5.0
+  m := 2.0
+
+  // Calculate the point-slope form of the line
+  y := m*(x1-x1) + y1
+
+  // Print the result
+  fmt.Printf("y - %.1f = %.1f(x - %.1f)\n", y1, m, x1)
+}
+/*
+In this example, the x1, y1, and m variables represent the coordinates of the point and the slope of the line, respectively. The y variable is calculated using the point-slope form of the line, and then the result is printed to the screen.
+
+You can use this same approach to calculate the point-slope form of a line for any given point and slope. Simply substitute the appropriate values for x1, y1, and m in the formula y - y1 = m(x - x1), and then use the math package to perform the calculations.
+*/
+
